c13cc9fba465780f64e12ee940416c68ecf46686
Joyce Ann Pestin <joycepestin@gmail.com>
2020-02-24 15:38:58 +0800

    Second Commit for OOP Prototype

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-class Student
+/*class Student
 {
     constructor(name, course, section)
     {
@@ -16,3 +16,19 @@ class Student
 
 let stud1 = new Student("Joyce Ann Pestin" , "IT" , "2ITF");
 stud1.enrolled("ICS2609");
+*/
+
+//Prototype - all the objects derived fmvthe same class can actually share
+
+let stud1 = {name: "Joyce Ann " , course: "IT" , section: "2ITF" , 
+study:function(subject)
+{
+    console.log(`${this.name}, is enrolled in ${subject}`);
+}};
+
+//calling the study method
+//use the objcect syntx
+
+let stud2 = {name: "Joyce" , course: "CS" , section: "2ITE"}; //di gagana kasi wlang study function
+
+stud1.study("ICS2609");
